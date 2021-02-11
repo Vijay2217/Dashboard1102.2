26f6397595cef3b481c749c05b1e1c6de2f309bd
Vijay2217 <vijay.skyindya@gmail.com>
2021-02-11 15:39:26 +0530

    v2

---
+++ Dashboard/Dash/Migrations/202102110807010_newLocalServer.cs
@@ -0,0 +1,16 @@
+﻿namespace Dash.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newLocalServer : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
